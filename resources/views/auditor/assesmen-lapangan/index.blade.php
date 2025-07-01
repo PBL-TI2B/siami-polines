d48a8149fb1e9270299b7ba16d417fadab804827
Miftachussurur <miftachussurur1993@gmail.com>
2025-07-01 11:07:29 +0700

    refactor(auditor): Improve getAuditingsByUser method and enhance UI for scheduling assessments

--- resources/views/auditor/assesmen-lapangan/index.blade.php
+++ resources/views/auditor/assesmen-lapangan/index.blade.php
@@ -2,53 +2,92 @@
 
 @section('title', 'Jadwalkan Asesmen Lapangan')
 
-{{-- @if(session('user'))
-    <meta name="user-id" content="{{ session('user')['user_id'] }}">
-@endif --}}
-
 @section('content')
-<div class="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
-    <!-- Breadcrumb -->
-    <x-breadcrumb :items="[
-        ['label' => 'Dashboard', 'url' => route('auditor.dashboard.index')],
-        ['label' => 'Audit', 'url' => route('auditor.audit.index')],
-        ['label' => 'Jadwalkan Assesmen Lapangan'],
-    ]" />
+<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 
-    <!-- Display Success Message -->
+ <x-breadcrumb :items="[
+    ['label' => 'Dashboard', 'url' => route('auditor.dashboard.index')],
+    ['label' => 'Audit', 'url' => route('auditor.audit.audit', ['id' => $auditing['auditing_id']])],
+    ['label' => 'Jadwal Assesmen Lapangan'],
+]" />
+    
+    <div class="flex justify-between items-center mb-6">
+        <h1 class="text-3xl font-bold text-gray-900 dark:text-white">
+            Edit Jadwal Asesmen Lapangan
+        </h1>
+    </div>
+
     @if (session('success'))
-        <div class="mb-4 rounded-lg bg-green-100 p-4 text-sm text-green-700 dark:bg-green-900 dark:text-green-300">
-            {{ session('success') }}
-        </div>
-    @endif
-
-    <!-- Display Error Message -->
-    @if (session('error'))
-        <div class="mb-4 rounded-lg bg-red-100 p-4 text-sm text-red-700 dark:bg-red-900 dark:text-red-300">
-            {{ session('error') }}
-        </div>
-    @endif
-
-    <h1 class="text-2xl font-semibold text-gray-900 dark:text-white mb-6">
-        Edit Jadwal Asesmen Lapangan
-    </h1>
-    <form action="{{ route('auditor.assesmen-lapangan.update', $auditing['auditing_id']) }}" method="POST" class="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        @csrf
-        @method('PUT')
-        <div class="mb-4">
-            <label for="jadwal_audit" class="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2">
-                Jadwal Asesmen Lapangan
-            </label>
-            <input type="date" id="jadwal_audit" name="jadwal_audit" required
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight dark:bg-gray-700 dark:text-white focus:outline-none focus:shadow-outline"
-                value="{{ isset($auditing['jadwal_audit']) ? \Carbon\Carbon::parse($auditing['jadwal_audit'])->format('Y-m-d') : '' }}"
-            >
-        </div>
-        <div class="flex items-center justify-between">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Simpan Jadwal
+        <div id="alert-3" class="flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div class="ms-3 text-sm font-medium">
+                {{ session('success') }}
+            </div>
+            <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-3" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
             </button>
         </div>
-    </form>
+    @endif
+
+    @if (session('error'))
+        <div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div class="ms-3 text-sm font-medium">
+                {{ session('error') }}
+            </div>
+            <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-2" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+            </button>
+        </div>
+    @endif
+
+
+    <div class="bg-white dark:bg-gray-800 shadow-xl rounded-lg overflow-hidden">
+        <div class="p-6 sm:p-8">
+            <form action="{{ route('auditor.assesmen-lapangan.update', $auditing['auditing_id']) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="space-y-6">
+                    <div>
+                        <label for="jadwal_audit" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Pilih Tanggal Asesmen Lapangan
+                        </label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4Z M0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                </svg>
+                            </div>
+                            <input type="date"
+                                   id="jadwal_audit"
+                                   name="jadwal_audit"
+                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                   value="{{ isset($auditing['jadwal_audit']) ? \Carbon\Carbon::parse($auditing['jadwal_audit'])->format('Y-m-d') : '' }}"
+                                   required
+                            >
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-8 flex items-center justify-end border-t border-gray-200 dark:border-gray-700 pt-6">
+                    <button type="submit" class="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-colors duration-200">
+                        <i class="fas fa-save mr-2"></i> Simpan Perubahan
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 @endsection
